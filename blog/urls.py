75abd516af39062a18049be47eecfd9f8806ae6f
Unknown <elioruiz.en@gmail.com>
2020-08-29 17:29:22 -0400

    Vista de lista como clase

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='blog'
 
 urlpatterns=[
-    path('',views.post_list,name='all'),
+    path('',views.PostList.as_view(),name='all'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import  Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
-def post_list(request):
-    posts=Post.published.all()#obtiene todos los post publicados
-    paginator=Paginator(posts,3)#tres post por pagina
-    page=request.GET.get('page')#obtiene el numero de la pagina
 
-    try:
-        posts=paginator.page(page)#obtengo los tres post de dicha pagina
-    except PageNotAnInteger:
-        #Si la page no es un entero entonces muestra la primera pagina 
-        posts=paginator.page(1)
-    except EmptyPage:
-        #Si page esta fuera de rango devuelve la ultima pagina
-        posts=paginator.page(paginator.num_pages)
-
-    '''
-    Since the Page object you are passing to the template is called posts, you include
-    the pagination template in the post list template, passing the parameters to render
-    it correctly.
-    '''
-
-    ctx={'posts':posts,'page':page}
-    return render(request,'blog/post/list.html',ctx)
+class PostList(ListView):
+    queryset = Post.published.all()#obtiene todos los post publicados
+    context_object_name = 'posts'
+    template_name='blog/post/list.html'
+    paginate_by=3
 
 def post_detail(request,year,month,day,post):
     post=get_object_or_404(Post,slug=post,
